423263a75922c8dea80300708403e03bf1b1cb35
Victor <victorzamoragutierrez@gmail.com>
2017-03-27 00:45:06 -0600

    Ya hay base de datos... estamos en el proceso de crear la conexión :P

--- src/Ciudad.java
+++ src/Ciudad.java
@@ -4,17 +4,19 @@
  * @version 1.0 
  */
 public class Ciudad{
+    public final String nombre; /* Nombre de la ciudad */
     public final double latitud; /* Coordenada de latitud */
     public final double longitud; /* Coordenada de longitud */
     public final int id; /* ID de la ciudad en la base de datos */
     
     /**
      * Constructor 
+     * @param nombre - El nombre de la ciudad
      * @param latitud - Coordenada de latitud
      * @param longitud - Coordenada de longitud
      * @param id - El id de la ciudad en la base de datos 
      */
-    public Ciudad(double latitud, double longitud, int id){
+    public Ciudad(String nombre, double latitud, double longitud, int id){
 	this.latitud = latitud;
 	this.longitud = longitud;
 	this.id = id;
--- src/TSP.java
+++ src/TSP.java
@@ -1,5 +1,26 @@
-/* Programa que utiliza recocido simulado para resolver el TSP 
- * (Traveling Salesman Problem */
+/**
+ * Programa que utiliza recocido simulado (Simulated Annealing) para resolver una versión modificada del TSP
+ * @author Victor Zamora Gutierrez
+ * @version 1.0
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class TSP{
+
     private Ciudad[] ciudades; /* Arreglo de ciudades */
+
+    /**
+     * Llena el arreglo de ciudades utilizando la base de datos 
+     */
+    public void llenaCiudades(){
+	Class.forName("org.sqlite.JDBC"); /* Carga el driver de sqlite */
+	Connection connection = null; /* Conexión a la base de datos */
+	connection = DriverManager.getConnection("jdbc:sqlite:sample.db"); /* Inicializamos la conexión */
+    }
+
 }
